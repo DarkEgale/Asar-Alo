e1a678c42a4c24649c369e055a83dd46b75001cc
Shimul <shimul@example.com>
2025-12-24 11:46:27 -0500

    bokacud amai

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter,Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/NavigationMenu/Navbar'
 import Home from './Pages/Home/Home'
 import FindCare from './Pages/FindCare/FindCare'
@@ -9,16 +9,15 @@ import Contact from './Pages/Contact/Contact'
 
 export default function App(){
   return (
-<HashRouter>
-  <Navbar />
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/find-care" element={<FindCare />} />
-    <Route path="/services" element={<Services />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-  </Routes>
-</HashRouter>
-
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/find-care" element={<FindCare />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
